ad7e04327234e4d4e27e17e32498718d3416514f
Andreas Umbach <umbach@gmail.com>
2016-07-28 22:17:06 +0200

    Initial Commit

---
+++ OpenGLReflectionDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OpenGLReflectionDemo
+//
+//  Created by Andreas Umbach on 28.07.2016.
+//  Copyright © 2016 Andreas Umbach. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OpenGLReflectionDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenGLReflectionDemo
+//
+//  Created by Andreas Umbach on 28.07.2016.
+//  Copyright © 2016 Andreas Umbach. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
